641df69a09fbc84e5998b178464ef06b95f50ee7
Sean <sean_developer@protonmail.com>
2023-08-17 00:03:46 -0400

    website finished implementing routes next

--- Final_Project_2/weather-app/src/App.js
+++ Final_Project_2/weather-app/src/App.js
@@ -1,15 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faCloud } from '@fortawesome/free-solid-svg-icons'
+import { faCloudRain } from '@fortawesome/free-solid-svg-icons'
 
 const api = {
   key: "117f2c5834f00373df4ace679e8aa5a6",
   base: "https://api.openweathermap.org/data/2.5/"
 }
 
+const weatherType = {
+  "Clouds": faCloud,
+  "Rain": faCloudRain
+}
+
 
 function App() {
+  const [query, setQuery] = useState('');
+  const [weather, setWeather] = useState({});
+  const [weatherBoxFadeOut, setWeatherBoxFadeOut] = useState(false);
+
+  const search = evt => {
+    if (evt.key === "Enter") {
+      setWeatherBoxFadeOut(true);
+      fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
+      .then(res => res.json())
+      .then(result => {
+        setWeather(result);
+        setWeatherBoxFadeOut(false);
+        setQuery('');
+        // console.log(result);
+        console.log(weatherType["Clouds"]);
+      });
+    }
+  }
+
+
+
   const dateBuilder = (d) => {
     let months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul","Aug", "Sep", "Oct", "Nov", "Dec"];
     let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
@@ -22,28 +48,42 @@ function App() {
     return `${day} ${date} ${month} ${year}`
   }
   return (
-    <div className="app">
+    <div className={(typeof weather.main != "undefined")
+    ? ((weather.main.temp > 16)
+    ? 'app warm'
+    : 'app')
+
+    : 'app'}>
       <main>
+
+      {/* <nav>
+            <ul>
+                <li>Home</li>
+                <li>Info</li>
+                <li>Contact</li>
+            </ul>
+        </nav> */}
+
         <div className="search-box">
           <input
             type="text"
             className="search-bar"
-            placeholder="Search for city"/>
+            placeholder="Search for city"
+            onChange={e => setQuery(e.target.value)}
+            value={query}
+            onKeyDown={search}
+            />
         </div>
         
-        <div className="location-box">
-        <div className="weather-icon"><FontAwesomeIcon icon={faCloud} /></div>
-        <div className="location">New York City, US</div>
-        <div className="date">{dateBuilder(new Date())}</div>
-        </div>
-
-
-        <div className="weather-box">
-          <div className="tempature">
-            15°
+        {weather && weather.main && (
+          <div className={`weather-box ${weatherBoxFadeOut ? 'fade-out' : 'fade-in'}`}>
+            <div className="weather-icon"><FontAwesomeIcon icon={weatherType[weather.weather[0].main]} /></div>
+            <div className="location">{weather.name}, {weather.sys && weather.sys.country}</div>
+            <div className="date">{dateBuilder(new Date())}</div>
+            <div className="tempature">{Math.round(weather.main.temp)}°</div>
+            <div className="weather">{weather.weather && weather.weather[0].main}</div>
           </div>
-          <div className="weather">Sunny</div>
-        </div>
+        )}
 
       </main>
     </div>
---
+++ Final_Project_2/weather-app/src/navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <ul>
+                <li>Home</li>
+                <li>Info</li>
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
